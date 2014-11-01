9b0d3ec9f387952cb46b06337c62ff7f9c6468ec
Cory Brown <oh.wise.man@gmail.com>
2014-11-01 14:51:56 -0600

    http

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'https://www.everydaydelicious.life/',
+        url: 'http://www.everydaydelicious.life/',
         mail: {
             transport: 'SMTP',
             options: {
